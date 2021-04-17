4fa3a44b20e627a7b1d2c6b81945f1d0f91590a6
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-04-16 23:54:16 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PraticandoDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
